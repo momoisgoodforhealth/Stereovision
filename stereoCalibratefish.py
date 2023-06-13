316f3339a0b1e8f261117810036b017e7035750c
Eric J Martin <emartin@mbari.org>
2023-06-13 12:03:31 -0700

    updates on calibration

--- stereoCalibratefish.py
+++ stereoCalibratefish.py
@@ -9,10 +9,13 @@ calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHEC
 objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
 objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 _img_shape = None
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-images = glob.glob('*.png')
-for fname in images:
+objpoints= [] # 3d point in real world space
+imgpointsL = [] # 2d points in image plane.
+imgpointsR=[]
+imagesL = glob.glob(r'C:\Users\Benjamin\Documents\Stereo-Vision\L\*.png')
+imagesR = glob.glob(r'C:\Users\Benjamin\Documents\Stereo-Vision\R\*.png')
+
+for fname in imagesL:
     img = cv2.imread(fname)
     if _img_shape == None:
         _img_shape = img.shape[:2]
@@ -25,25 +28,50 @@ for fname in images:
     if ret == True:
         objpoints.append(objp)
         cv2.cornerSubPix(gray,corners,(3,3),(-1,-1),subpix_criteria)
-        imgpoints.append(corners)
+        imgpointsL.append(corners)
+
+for fname in imagesR:
+    img = cv2.imread(fname)
+    if _img_shape == None:
+        _img_shape = img.shape[:2]
+    else:
+        assert _img_shape == img.shape[:2], "All images must share the same size."
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        cv2.cornerSubPix(gray,corners,(3,3),(-1,-1),subpix_criteria)
+        imgpointsR.append(corners)
+
 N_OK = len(objpoints)
-K = np.zeros((3, 3))
-D = np.zeros((4, 1))
+K1 = np.zeros((3, 3))
+K2 = np.zeros((3, 3))
+D1 = np.zeros((4, 1))
+D2 = np.zeros((4, 1))
 rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
 tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+R = np.zeros((1, 1, 3), dtype=np.float64)
+T = np.zeros((1, 1, 3), dtype=np.float64)
 rms, _, _, _, _ = \
     cv2.fisheye.stereoCalibrate(
         objpoints,
-        imgpoints,
-        gray.shape[::-1],
-        K,
-        D,
-        rvecs,
-        tvecs,
+        imgpointsL,
+        imgpointsR, 
+        K1,
+        D1,
+        K2,
+        D2,
+        gray.shape[::-1],R,T,
+     #  rvecs,
+     #   tvecs,
         calibration_flags,
         (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
     )
 print("Found " + str(N_OK) + " valid images for calibration")
 print("DIM=" + str(_img_shape[::-1]))
-print("K=np.array(" + str(K.tolist()) + ")")
-print("D=np.array(" + str(D.tolist()) + ")")
+print("K1=np.array(" + str(K1.tolist()) + ")")
+print("D1=np.array(" + str(D1.tolist()) + ")")
+print("K1=np.array(" + str(K2.tolist()) + ")")
+print("D1=np.array(" + str(D2.tolist()) + ")")
